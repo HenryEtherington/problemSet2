d817f975ede2e875c759c1c7d151bb07de4d3b11
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-25 17:45:39 +0100

    Add files via upload

---
+++ PeopleTest.java
@@ -0,0 +1,10 @@
+
+import javax.swing.*;
+
+public class PeopleTest {
+    public static void main(String[] args) {
+        People p = new People();
+
+        JOptionPane.showMessageDialog(null,p.toString());
+    }
+}
