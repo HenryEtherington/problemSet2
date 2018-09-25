564bc0a04dc658a8be21add8ae89bda5aade278b
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-25 17:45:54 +0100

    Add files via upload

---
+++ PeopleDriver.java
@@ -0,0 +1,9 @@
+
+import javax.swing.*;
+
+public class PeopleDriver {
+    public static void main(String[] args) {
+        People people = new People();
+        JOptionPane.showMessageDialog(null,people.toString());
+    }
+}
