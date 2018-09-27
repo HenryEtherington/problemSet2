2d03b8d2a5c8d0e03d38f4b1c69b4f6ef9da6fa4
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-27 12:46:35 +0100

    Add files via upload

---
+++ Ex1/Book.java
@@ -0,0 +1,60 @@
+public class Book {
+
+
+    String title;
+    double price;
+    int ISBN, numberOfPages;
+
+    //Accessor methods
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public float getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    //muatator methods
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setNumberOfPages(int numberOfpages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    //constructor methods
+
+    public Book() {
+        this.title = "The Lime Machine";
+        this.price = '8';
+        this.ISBN = '1';
+        this.numberOfPages = '5';
+    }
+
+    public String BooktoString() {
+        return "Title" + title + "\nPrice" + price + "\nISBN" + ISBN + "\nNumber of pages" + numberOfPages;
+    }
+
+}
+
+
+
