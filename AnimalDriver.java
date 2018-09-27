01fcd7b21fad3bcc1480679ec68dfc858103a808
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-27 12:47:11 +0100

    Delete AnimalDriver.java

--- AnimalDriver.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.*;
-
-public class AnimalDriver{
-    public static void main(String[] args) {
-        Animal animal = new Animal();
-        JOptionPane.showMessageDialog(null,animal.toString());
-    }
-}
