dbbb5c737c1961bb2f1082842c8d9ae73ec578b6
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-27 12:45:59 +0100

    Add files via upload

---
+++ AnimalDriver.java
@@ -0,0 +1,9 @@
+
+import javax.swing.*;
+
+public class AnimalDriver{
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        JOptionPane.showMessageDialog(null,animal.toString());
+    }
+}
