0090ced6d35089afa1e0129a4d1caae29b5a46b9
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-27 12:48:03 +0100

    Delete PeopleDriver.java

--- PeopleDriver.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.*;
-
-public class PeopleDriver {
-    public static void main(String[] args) {
-        People people = new People();
-        JOptionPane.showMessageDialog(null,people.toString());
-    }
-}
