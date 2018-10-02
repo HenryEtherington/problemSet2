d1b9c2cbc2c331992acbdac1cb5bfe3c61269738
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-10-02 17:47:03 +0100

    Add files via upload

---
+++ Ex5/bicycle.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+
+public class bicycle{
+    public static void main(String[] args) {
+
+      String name = JOptionPane.showInputDialog("Please enter your name");
+      String value = JOptionPane.showInputDialog("Please enter your value");
+
+
+                }
+         }
---
+++ Ex5/bicycleDriver.java
@@ -0,0 +1,9 @@
+
+import javax.swing.*;
+
+public class bicyclelDriver{
+    public static void main(String[] args) {
+        bicycle b = new bicycle();
+        JOptionPane.showMessageDialog(null,bicycletoString());
+    }
+}
