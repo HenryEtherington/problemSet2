80245e45794ea1df9ffc759f1c02b48eee9c269d
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-10-04 12:36:42 +0100

    Add files via upload

--- Ex5/bicycleDriver.java
+++ Ex5/bicycleDriver.java
@@ -1,9 +1,19 @@
 
 import javax.swing.*;
 
-public class bicyclelDriver{
+public class bicycleDriver {
     public static void main(String[] args) {
-        bicycle b = new bicycle();
-        JOptionPane.showMessageDialog(null,bicycletoString());
+        String name, valueAsString;
+
+        bicycle [] b = new bicycle[3];
+
+        //enhanced for loop
+        for (b: bicycle){
+            String name = JOptionPane.showInputDialog("Please enter your name");
+            String valueAsString = JOptionPane.showInputDialog("Please enter your value");
+            b = new bicycle(name,valueAsString);
+            System.out.println(b.toString());
+        }
     }
 }
+
