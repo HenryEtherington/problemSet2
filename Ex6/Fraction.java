9b406041bf14532ef5e18f9ff634e21291249b75
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-10-04 15:46:35 +0100

    Add files via upload

---
+++ Ex6/Fraction.java
@@ -0,0 +1,16 @@
+
+public class Fraction {
+
+int x = 2, y = 3;
+
+public void setX(int x) {this.x = x;}
+public int getX() {return x;}
+public void setY(int y) { this.y= y;}
+public int getY(){return y;}
+
+
+    @Override
+    public String toString()  {return "Name" + getX() + "\nValue" + getY();}
+
+
+}
---
+++ Ex6/FractionDriver.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class FractionDriver {
+    public static void main(String[] args){
+
+String x, y;
+
+Fraction [] f = new Fraction[3];
+
+            System.out.println(f.toString());
+
+    }
+}
